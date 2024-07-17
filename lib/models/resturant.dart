fc22efba9e1fb321702101180e62e32bc3a1261e
Kentswindells <kentswindells@gmail.com>
2024-07-17 14:28:11 +0000

    FOOD CAT. DONE

--- lib/models/resturant.dart
+++ lib/models/resturant.dart
@@ -136,11 +136,11 @@ class Resturant {
     //sides
     Food(
       name: "Bagels",
-      description:
-          "Crispy chicken with lecttuce",
-      imagepath: "lib/images/burgers/bagels-with-lettuce-tomatoes_144627-17178.jpg",
+      description: "Crispy chicken with lecttuce",
+      imagepath:
+          "lib/images/burgers/bagels-with-lettuce-tomatoes_144627-17178.jpg",
       price: 25.10,
-      category: FoodCategory.sides, 
+      category: FoodCategory.sides,
       availableAddons: [
         Addon(name: "cheese sauce ", price: 2.20),
         Addon(name: "Truffle oil", price: 5.20),
@@ -149,8 +149,7 @@ class Resturant {
     ),
     Food(
       name: "Club Sandwich",
-      description:
-          "Enjoy a tasty delight flavour of a club sandwich",
+      description: "Enjoy a tasty delight flavour of a club sandwich",
       imagepath: "lib/images/burgers/club-sandwich.jpg",
       price: 5.10,
       category: FoodCategory.sides,
@@ -164,7 +163,8 @@ class Resturant {
       name: "Grilled Chicken Steak",
       description:
           "Smoky Chicken,with onion rings make this sides savory delight",
-      imagepath: "lib/images/burgers/grilled-chicken-steak-with-french-fries.jpg",
+      imagepath:
+          "lib/images/burgers/grilled-chicken-steak-with-french-fries.jpg",
       price: 25.10,
       category: FoodCategory.sides,
       availableAddons: [
@@ -175,8 +175,7 @@ class Resturant {
     ),
     Food(
       name: "Grilled Chicken Steak",
-      description:
-          "Tasty chicken with soft lecttuce",
+      description: "Tasty chicken with soft lecttuce",
       imagepath: "lib/images/burgers/grilled-chicken-steak.jpg",
       price: 5.10,
       category: FoodCategory.sides,
@@ -201,7 +200,146 @@ class Resturant {
     ),
 
     //desserts
+    Food(
+      name: "Cupcake",
+      description: "Expeirence the floffy texture and a savory taste",
+      imagepath: "lib/images/burgers/cupcakes.jpg",
+      price: 15.10,
+      category: FoodCategory.desserts,
+      availableAddons: [
+        Addon(name: "Extra Vanilla", price: 2.20),
+        Addon(name: "Raspberry sauce", price: 5.20),
+        Addon(name: "Honey", price: 10.20)
+      ],
+    ),
+    Food(
+      name: "Delicious donut",
+      description: "Spicy chocolate toppings that comes with a tasty delight",
+      imagepath: "lib/images/burgers/delicious-donuts.jpg",
+      price: 25.10,
+      category: FoodCategory.desserts,
+      availableAddons: [
+        Addon(name: "Extra Topping", price: 2.20),
+        Addon(name: "Extra Chocolate", price: 5.20),
+        Addon(name: "Vanilla", price: 10.20)
+      ],
+    ),
+    Food(
+      name: "Dessert fruit cake",
+      description:
+          "Enjoy a floffy taste of chololate with awesome raspberry sauce",
+      imagepath: "lib/images/burgers/dessert-fruitcake.jpg",
+      price: 25.10,
+      category: FoodCategory.desserts,
+      availableAddons: [
+        Addon(name: "Extra chocolate toppings", price: 2.20),
+        Addon(name: "Honey", price: 5.20),
+        Addon(name: "Extra Vanilla ", price: 10.20)
+      ],
+    ),
+    Food(
+      name: "Strawberry Cake",
+      description: "Savory taste of strawberry with icy cake",
+      imagepath: "lib/images/burgers/small-cake-strawberries.jpg",
+      price: 25.10,
+      category: FoodCategory.desserts,
+      availableAddons: [
+        Addon(name: "Extra Vanilla", price: 2.20),
+        Addon(name: "Cake Topping", price: 5.20),
+        Addon(name: "Honey", price: 10.20)
+      ],
+    ),
+    Food(
+      name: "Sweet Chocolate Cake",
+      description:
+          "Having a sweet tooth? creamy chocolate cake to more than satisfy your sweet tooth cake.",
+      imagepath: "lib/images/burgers/sweet-chocolate.jpg",
+      price: 25.10,
+      category: FoodCategory.desserts,
+      availableAddons: [
+        Addon(name: "Vanilla", price: 2.20),
+        Addon(name: "Extra cream", price: 5.20),
+        Addon(name: "Honey", price: 10.20),
+      ],
+    ),
 
     //drinks
+    Food(
+      name: "",
+      description:
+          "Chilled ice drink with a hint of lemon, served over ice.",
+      imagepath: "lib/images/burgers/ice-drinking.jpg",
+      price: 25.10,
+      category: FoodCategory.drinks,
+      availableAddons: [
+        Addon(name: "Orange flavor", price: 2.20),
+        Addon(name: "Lemon Slice", price: 5.20),
+        Addon(name: "Honey", price: 10.20),
+      ],
+    ),
+    Food(
+      name: "Iced Cocktail ",
+      description:
+          "A blend of natural fruit cocktail, for a healthy boost.",
+      imagepath: "lib/images/burgers/iced-cocktail.jpg",
+      price: 25.10,
+      category: FoodCategory.drinks,
+      availableAddons: [
+        Addon(name: "Protein powder", price: 2.20),
+        Addon(name: "Extra creamy taste", price: 5.20),
+        Addon(name: "Honey", price: 10.20),
+      ],
+    ),
+    Food(
+      name: "Lemonade",
+      description:
+          "A fresh lemonade to chill and relax your discomfort.",
+      imagepath: "lib/images/burgers/lemonade.jpg",
+      price: 25.10,
+      category: FoodCategory.drinks,
+      availableAddons: [
+        Addon(name: "Protein Powder", price: 2.20),
+        Addon(name: "Almond Milk", price: 5.20),
+        Addon(name: "Honey", price: 10.20),
+      ],
+    ),
+    Food(
+      name: "Orange Soft Yellow Drink ",
+      description:
+          "A classic Cuban drink with muddled lime and mint, sweetned with sugar.",
+      imagepath: "lib/images/burgers/orange-soft-yellow-drinks.jpg",
+      price: 25.10,
+      category: FoodCategory.drinks,
+      availableAddons: [
+        Addon(name: "Extra Mint", price: 2.20),
+        Addon(name: "Raspberry Puree", price: 5.20),
+        Addon(name: "Honey", price: 10.20),
+      ],
+    ),
+    Food(
+      name: "Refreshing Drink",
+      description:
+          "A classic Refreshing drink to sooth your discomfort",
+      imagepath: "lib/images/burgers/sweet-chocolate.jpg",
+      price: 25.10,
+      category: FoodCategory.drinks,
+      availableAddons: [
+        Addon(name: "Extra Mint", price: 2.20),
+        Addon(name: "lemon", price: 5.20),
+        Addon(name: "Honey", price: 10.20),
+      ],
+    ),
   ];
+
+  /*
+  G E T T E R S
+  */
+
+   /*
+  O P E R A T I O N S
+  */
+
+   /*
+  H E L P E R S
+  */
 }
